c826b40b25c327f134735025334cfe33b6d0767a
Majid Akbari <majid.akbari@neocard.fi>
2022-04-02 13:46:33 +0430

    add more tests

--- app/__tests__/feature/http/errorHandlingTest.ts
+++ app/__tests__/feature/http/errorHandlingTest.ts
@@ -9,15 +9,49 @@ beforeAll(async () => {
     server = createServer();
 })
 
-describe('GET /api/hello', () => {
-    it('should return 200 & valid response', done => {
+describe('show proper error messages in case of having invalid request.', () => {
+    it('should return 404 when route does not exist.', done => {
         request(server)
-            .get(`/api/hello`)
+            .get(`/foo`)
             .expect('Content-Type', /json/)
-            .expect(200)
+            .expect(404)
             .end((err, res) => {
                 if (err) return done(err)
-                expect(res.body).toMatchObject({'message': 'Hello world'})
+                expect(res.body).toMatchObject({
+                    error: "Client Error",
+                    message: "Route not found."
+                })
+                done()
+            })
+    })
+
+    it('should return 406 when accept header is not supported by the app.', done => {
+        request(server)
+            .get(`/foo`)
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /json/)
+            .expect(406)
+            .end((err, res) => {
+                if (err) return done(err)
+                expect(res.body).toMatchObject({
+                    error: "Client Error",
+                    message: "This application only returns json responses."
+                })
+                done()
+            })
+    })
+
+    it('should return 405 when request method is not correct.', done => {
+        request(server)
+            .post(`/api/hello`)
+            .expect('Content-Type', /json/)
+            .expect(405)
+            .end((err, res) => {
+                if (err) return done(err)
+                expect(res.body).toMatchObject({
+                    error: "Client Error",
+                    message: "Method not allowed."
+                })
                 done()
             })
     })
