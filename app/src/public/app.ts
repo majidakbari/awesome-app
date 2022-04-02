9f1f4033ff05bb7a7cf5080fde29f8836293d27d
Majid Akbari <majid.akbari@neocard.fi>
2022-04-02 11:32:52 +0430

    add application entry point

---
+++ app/src/public/app.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata"
+import dotenv from "dotenv";
+import helmet from "helmet";
+import express from "express";
+import appRouter from "../routes/index";
+import corsHandler from "../middlewares/corsHandler";
+import errorHandler from "../middlewares/errorHandler";
+import routeHandler from "../middlewares/routeHandler";
+import acceptableHandler from "../middlewares/acceptableHandler";
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(corsHandler);
+app.use(acceptableHandler);
+app.use(appRouter);
+app.use(routeHandler);
+app.use(errorHandler);
+app.listen(process.env.NODE_PORT)
