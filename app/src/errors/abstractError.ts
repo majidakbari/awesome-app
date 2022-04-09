265b387429ad00b0048b110dabc0c17e2332550e
Majid Akbari <majid.akbari@neocard.fi>
2022-04-09 10:10:42 +0430

    better design in error handling

--- app/src/errors/abstractError.ts
+++ app/src/errors/abstractError.ts
@@ -1,13 +1,10 @@
-import {ValidationError} from "express-validator";
-
 abstract class AbstractError extends Error {
-    protected constructor(private statusCode: number, message: string, private details: ValidationError[] = []) {
+    protected constructor(protected statusCode: number, message: string) {
         super(message);
     }
 
     public getMessage = (): string => this.message;
     public getStatusCode = (): number => this.statusCode;
-    public getDetails: () => ValidationError[] = () => this.details;
 }
 
 export default AbstractError;
--- app/src/errors/clinetErrors/abstractClientError.ts
+++ app/src/errors/clinetErrors/abstractClientError.ts
@@ -1,6 +1,12 @@
 import AbstractError from "../abstractError";
+import {ValidationError} from "express-validator";
 
 abstract class AbstractClientError extends AbstractError {
+    protected constructor(protected statusCode: number, message: string, protected details: ValidationError[] = []) {
+        super(statusCode, message);
+    }
+    public getDetails: () => ValidationError[] = () => this.details;
+
 }
 
 export default AbstractClientError;
