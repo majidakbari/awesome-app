581d498c7a6f87e5ce78f769c405c63918c2263c
Majid Akbari <majid.akbari@neocard.fi>
2022-04-11 18:08:40 +0430

    integration tests for replyAddedHandlerService.test.ts

--- app/src/__tests__/integration/services/replyAddedHandlerService.test.ts
+++ app/src/__tests__/integration/services/replyAddedHandlerService.test.ts
@@ -1,6 +1,17 @@
 import {closeConnection, userTagRepository} from "../../../integrations/dbConnection";
+import replyAddedHandlerService from "../../../services/replyAddedHandlerService";
+import Event from "../../../interfaces/event";
+import assert = require("assert");
 
-describe('attach tag to user service should act as expected.', () => {
+const getPostTagsByIdService = require("../../../services/getPostTagsByIdService")
+
+jest.mock("../../../services/getPostTagsByIdService", () => jest.fn());
+
+beforeEach(() => {
+    getPostTagsByIdService.mockClear();
+});
+
+describe('reply added handler service should work as expected.', () => {
 
     beforeAll(async (): Promise<void> => {
         await (await userTagRepository()).clear();
@@ -14,7 +25,45 @@ describe('attach tag to user service should act as expected.', () => {
         await closeConnection();
     });
 
-    it('should do nothing when userTag record already exists.', async () => {
+    it('should find the post tags through graphql and attach it to the user who left the reply.', async () => {
+        const postTags = [{title: 'foo'}, {title: 'bar'}, {title: 'buzz'}];
+        const postId = 'some-post-id';
+        const actorId = 'sth-id';
 
+        getPostTagsByIdService.mockImplementation(async () => Promise.resolve(postTags));
+
+        const event = {
+            eventType: 'reply.added',
+            eventBody: {
+                object: {
+                    postId: postId
+                },
+                actor: {
+                    id: actorId
+                }
+            }
+        } as Event;
+
+        await replyAddedHandlerService(event);
+
+        const userTags = await (await userTagRepository()).find({where: {userId: actorId}});
+
+        assert.equal(userTags?.length, 3);
+        expect(getPostTagsByIdService).toBeCalledWith(postId);
+        expect(getPostTagsByIdService).toHaveBeenCalled();
+    });
+
+    it('should do nothing when event structure is not correct.', async () => {
+        const event = {
+            eventType: 'reply.added',
+            eventBody: {}
+        } as Event;
+
+        await replyAddedHandlerService(event);
+
+        const userTags = await (await userTagRepository()).find();
+
+        assert.equal(userTags?.length, 0);
+        expect(getPostTagsByIdService).not.toHaveBeenCalled();
     });
 });
